37ad6182956d4fdf5203c6db8eb77cab1387f9cc
TexugoGaio <felipe.andoliv@gmail.com>
2017-10-19 18:25:16 -0200

    ....

--- loja.sql
+++ loja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 30-Set-2017 às 02:30
+-- Generation Time: 17-Set-2017 às 16:02
 -- Versão do servidor: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -30,19 +30,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `clientes` (
   `id` int(11) NOT NULL,
-  `nome` varchar(40) NOT NULL,
-  `cpf` bigint(12) NOT NULL,
-  `cidade` varchar(40) NOT NULL,
-  `estado` char(2) NOT NULL
+  `nome` varchar(35) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `telefone` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `clientes`
---
-
-INSERT INTO `clientes` (`id`, `nome`, `cpf`, `cidade`, `estado`) VALUES
-(2, 'Felipe Andrade de Oliveira', 45239419809, 'Assis', 'SP'),
-(3, 'Izabel Cristina Andrade', 1518392806, 'Assis', 'SP');
 
 -- --------------------------------------------------------
 
@@ -52,46 +43,9 @@ INSERT INTO `clientes` (`id`, `nome`, `cpf`, `cidade`, `estado`) VALUES
 
 CREATE TABLE `produtos` (
   `id` int(11) NOT NULL,
-  `descricao` varchar(100) NOT NULL,
+  `descricao` varchar(35) NOT NULL,
   `quantidade` float NOT NULL,
   `unidade` varchar(35) NOT NULL,
-  `valor` float(6,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `produtos`
---
-
-INSERT INTO `produtos` (`id`, `descricao`, `quantidade`, `unidade`, `valor`) VALUES
-(1, 'Processador - Intel - i7 7700k', 160, 'un', 1500.00),
-(2, 'Processador - Intel - i7 6600', 40, 'un', 1100.00),
-(3, 'MemÃ³ria - Corsair - 16Gb 2x8', 100, 'un', 400.00),
-(5, 'Placa de VÃ­deo - Gigabyte - GTX 1080 TI - 11 GB', 200, 'un', 3500.00);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `produto_venda`
---
-
-CREATE TABLE `produto_venda` (
-  `venda_id` int(11) NOT NULL,
-  `prod_id` int(11) NOT NULL,
-  `valor_uni` float NOT NULL,
-  `valor_total` float NOT NULL,
-  `quantidade` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `vendas`
---
-
-CREATE TABLE `vendas` (
-  `venda_id` int(11) NOT NULL,
-  `cli_id` int(11) NOT NULL,
-  `data` date NOT NULL,
   `valor` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -112,20 +66,6 @@ ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `produto_venda`
---
-ALTER TABLE `produto_venda`
-  ADD PRIMARY KEY (`venda_id`,`prod_id`),
-  ADD KEY `prod_id` (`prod_id`);
-
---
--- Indexes for table `vendas`
---
-ALTER TABLE `vendas`
-  ADD PRIMARY KEY (`venda_id`),
-  ADD KEY `cliente_venda_fk` (`cli_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -133,34 +73,12 @@ ALTER TABLE `vendas`
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `vendas`
---
-ALTER TABLE `vendas`
-  MODIFY `venda_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `produto_venda`
---
-ALTER TABLE `produto_venda`
-  ADD CONSTRAINT `produto_venda_ibfk_1` FOREIGN KEY (`venda_id`) REFERENCES `vendas` (`venda_id`),
-  ADD CONSTRAINT `produto_venda_ibfk_2` FOREIGN KEY (`prod_id`) REFERENCES `produtos` (`id`);
-
---
--- Limitadores para a tabela `vendas`
---
-ALTER TABLE `vendas`
-  ADD CONSTRAINT `cliente_venda_fk` FOREIGN KEY (`cli_id`) REFERENCES `clientes` (`id`);
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
